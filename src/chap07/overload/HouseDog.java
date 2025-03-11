6551ce0e5c77c3a927b397e80a976c589251bd2b
rma4921 <rma4921@naver.com>
2025-03-11 17:14:28 +0900

    add main

--- src/chap07/overload/HouseDog.java
+++ src/chap07/overload/HouseDog.java
@@ -4,4 +4,10 @@ public class HouseDog extends Dog{
     void sleep(int hour) {
         System.out.println(name + "Zzz... for " + hour);
     }
+    public static void main(String[] args) {
+        HouseDog houseDog = new HouseDog();
+        houseDog.setName("puppy");
+        houseDog.sleep();
+        houseDog.sleep(3);
+    }
 }
