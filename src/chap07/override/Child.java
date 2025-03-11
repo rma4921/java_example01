f3cb0e7f0a14d87d35e42dbcae7aca3b4fab5071
rma4921 <rma4921@naver.com>
2025-03-11 17:07:26 +0900

    Correct typo

--- src/chap07/override/Child.java
+++ src/chap07/override/Child.java
@@ -3,7 +3,7 @@ package chap07.override;
 public class Child extends Parent{
     String field2;
     // Override는 부모 메소드와 동일한 시그니처(리턴타입, 메소드이름, 매개변수)를 가져야 한다.
-    // 단, 접근제한자는 부모보다 강력해질 수 없다. ex) private -> protected (x), default -> public (o)
+    // 단, 접근제한자는 부모보다 강력해질 수 없다. ex) public -> protected (x), default -> public (o)
     @Override
     void method2() {
         System.out.println("Child.method2()");
